e73991dc21baa5864cc0857d672c8f95880df990
yecheng <leaf_city@163.com>
2015-03-15 22:29:25 +0800

    ..

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,54 +1,112 @@
-function $ (s) {
+function $(s) {
 	return document.querySelectorAll(s);
 }
 
 var lis = $("#list li");
-for(var i=0;i<lis.length;i++){
-	lis[i].onclick = function(){
-		for(var j=0;j<lis.length;j++){
+var size = 32;
+var box = $("#box")[0];
+var height, width;
+var canvas = document.createElement("canvas");
+var ctx = canvas.getContext("2d");
+box.appendChild(canvas);
+var Dots = [];
+var line;
+var mv = new MusicVisualizer({
+	size: size,
+	visualizer: draw
+})
+
+for (var i = 0; i < lis.length; i++) {
+	lis[i].onclick = function() {
+		for (var j = 0; j < lis.length; j++) {
 			lis[j].className = "";
 		}
 		this.className = "selected";
-		load("/media/"+this.title);
+		mv.play("/media/" + this.title);
 	}
 }
 
-var xhr = new XMLHttpRequest();
-var ac = new (window.AudioContext||window.webkitAudioContext)();
-var gainNode = ac[ac.createGain?"createGain":"createGainNode"]();
-gainNode.connect(ac.destination);
-var source = null;
-var count = 0;
+function random(m, n) {
+	return Math.round(Math.random() * (n - m) + m);
+}
 
-function load(url){
-	var n = ++count;
-	source && source[source.stop?"stop":"noteOff"]();
-	xhr.abort();
-	xhr.open("GET",url);
-	xhr.responseType = "arraybuffer";
-
-	xhr.onload = function(){
-		if(n != count) return;
-		ac.decodeAudioData(xhr.response,function(buffer){
-			if(n != count) return;
-			var bufferSource = ac.createBufferSource();
-			bufferSource.buffer = buffer;
-			bufferSource.connect(gainNode);
-			bufferSource[bufferSource.start?"start":"noteOn"](0);
-			source = bufferSource;
-		},function(err){
-			console.log(err);
+function getDots() {
+	Dots = [];
+	for (var i = 0; i < size; i++) {
+		var x = random(0, width);
+		var y = random(0, height);
+		var color = "rgba(" + random(0, 255) + "," + random(0, 255) + "," + random(0, 255) + ",0)";
+		Dots.push({
+			x: x,
+			y: y,
+			dx: random(1, 4),
+			color: color,
+			cap: 0
 		});
 	}
-	xhr.send(null);
 }
 
-function changeVolume(percent){
-	gainNode.gain.value = percent*percent;
-	console.log(percent);
+function resize() {
+	height = box.clientHeight;
+	width = box.clientWidth;
+	canvas.height = height;
+	canvas.width = width;
+	line = ctx.createLinearGradient(0, 0, 0, height);
+	line.addColorStop(0, "red");
+	line.addColorStop(0.5, "yellow");
+	line.addColorStop(1, "green");
+	getDots();
 }
+resize();
+window.onresize = resize;
 
-$("#volume")[0].onchange = function(){
-	changeVolume(this.value/this.max);
+function draw(arr) {
+	ctx.fillStyle = line;
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	var w = width / size;
+	var cw = w * 0.6;
+	var capH = cw > 10 ? 10 : cw;
+	for (var i = 0; i < size; i++) {
+		var o = Dots[i];
+		if (draw.type == "column") {
+			var h = arr[i] / 256 * height;
+			ctx.fillRect(w * i, height - h, w * 0.6, h);
+			ctx.fillRect(w * i, height - (o.cap + capH), cw, capH);
+			o.cap--;
+			if (o.cap < 0) {
+				o.cap = 0;
+			}
+			if (h > 0 && o.cap < h + 40) {
+				o.cap = h + 40 > height - capH ? height - capH : h + 40;
+			}
+		} else if (draw.type == "dot") {
+			ctx.beginPath();
+			var r = 10 + arr[i] / 256 * (height > width ? width : height) / 10;
+			ctx.arc(o.x, o.y, r, 0, Math.PI * 2, true);
+			var g = ctx.createRadialGradient(o.x, o.y, 0, o.x, o.y, r);
+			g.addColorStop(0, "#fff");
+			g.addColorStop(1, o.color);
+			ctx.fillStyle = g;
+			ctx.fill();
+			o.x += o.dx;
+			o.x = o.x > width ? 0 : o.x;
+		}
+	}
+}
+draw.type = "column";
+
+$("#volume")[0].onchange = function() {
+	mv.changeVolume(this.value / this.max);
 }
 $("#volume")[0].onchange();
+
+var ts = $("#type li");
+for (var i = 0; i < ts.length; i++) {
+	ts[i].onclick = function() {
+		for (var j = 0; j < ts.length; j++) {
+			ts[j].className = "";
+		}
+		this.className = "selected";
+		draw.type = this.dataset.type;
+	}
+}
